61f3b014082349ed51fbd582eeb72861f8324c70
AustinA86 <austinmasila@gmail.com>
2023-09-21 10:25:00 +0530

    This is sample frontend programs

---
+++ app.ts
@@ -0,0 +1,7 @@
+// Declare a variable message of type string
+let message: string = "Hello World !!!";
+
+// Print the message to the console
+console.log(message);
+
+document.getElementById("ak").innerHTML=message;
